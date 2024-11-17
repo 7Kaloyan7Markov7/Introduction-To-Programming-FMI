7d4defe47a43fa4a81a8a1286e8955ffd91a0767
Kalata <153134271+KokoBoko04@users.noreply.github.com>
2024-11-17 14:10:55 +0200

    Update isSubArray.cpp

--- week 05/isSubArray.cpp
+++ week 05/isSubArray.cpp
@@ -2,24 +2,29 @@
 
 const int MAX_SIZE = 100;
 
-bool isSubArray(const int arr1[MAX_SIZE], const int arr2[MAX_SIZE],int size1, int size2)
+bool isSubArray(const int arr1[MAX_SIZE], const int arr2[MAX_SIZE], int size1, int size2)
 {
-	bool hasMetElement = false;
-	for (int i = 0; i < size2; ++i)
-	{
-		for (int j = 0; j < size1; ++j)
-		{
-			if (arr2[i] == arr1[j])
-			{
-				hasMetElement = true;
-			}
-		}
-		if (!hasMetElement)
-		{
-			return false;
-		}
-		hasMetElement = false;
-	}
+    if (size1 <= 0 || size2 <= 0 || size1 > MAX_SIZE || size2 > MAX_SIZE)
+    {
+        return false;
+    }
 
-	return true;
+    for (int i = 0; i < size2; ++i)
+    {
+        bool found = false;
+        for (int j = 0; j < size1; ++j)
+        {
+            if (arr2[i] == arr1[j])
+            {
+                found = true;
+                break; 
+            }
+        }
+        if (!found)
+        {
+            return false;
+        }
+    }
+
+    return true; 
 }
