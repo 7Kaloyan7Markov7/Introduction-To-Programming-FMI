9969bb3141d8af667d9963859eb16c939138deda
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2024-12-06 17:57:01 +0200

    Update invertKthBit.cpp

--- Week 8/invertKthBit.cpp
+++ Week 8/invertKthBit.cpp
@@ -2,11 +2,9 @@
 
 unsigned invertKthBit(unsigned n, int k)
 {
-	int mask = 1;
-	for (int i = 0; i < k; ++i)
-	{
-		mask = mask << 1;
-	}
+	if (k < 0) return -1;
+
+	int mask = 1 << k;
 
 	if ((n >> k) & 1) return (n & ~mask);
 	else return (n | mask);
