ac36915c74264ce83906ff0287bd0f9f26782266
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2024-12-11 03:13:54 +0200

    Update countOfNumSymbs.cpp

--- Week 9/countOfNumSymbs.cpp
+++ Week 9/countOfNumSymbs.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+int getPowered(int n, int power)
+{
+	int result = 1;
+	for (int i = 0; i < power; ++i)
+	{
+		result *= n;
+	}
+	return result;
+}
+
+int countOfDigits(int num)
+{
+	if (!num) return 1;
+	int counter = 0;
+
+	while (num != 0)
+	{
+		num /= 10;
+		counter++;
+	}
+	return counter;
+}
+
 char numToSymb(int num)
 {
 	if (num >= 0 && num <= 9)
@@ -13,22 +36,32 @@ void countOfNumSymbs(const char str[], char result[])
 	
 	while (str[index] != '\0')
 	{
-		if (str[index] >= '0' && str[index] <= '9')
-			hist[str[index] - '0']++;
-		else
+		if (str[index] < '0' || str[index] > '9')
 		{
 			std::cout << "Invalid input";
 			return;
 		}
-		index++;
+		hist[str[index++] - '0']++;
 	}
+
 	int resIndex = 0;
 
 	for (int i = 0; i < 10; ++i)
 	{
 		if (!hist[i]) continue;
-		
+		int digitCount = countOfDigits(hist[i]);
+
+		if(hist[i] < 10)
 		result[resIndex++] = numToSymb(hist[i]);
+
+		if(hist[i] >= 10)
+		for (int j = 0; j < digitCount; ++j)
+		{
+			int extractor = getPowered(10, digitCount - j - 1);
+			result[resIndex++] = numToSymb(hist[i] / extractor);
+			hist[i] %= extractor;
+		}
+
 		result[resIndex++] = 'x';
 		result[resIndex++] = numToSymb(i);
 		result[resIndex++] = ',';
