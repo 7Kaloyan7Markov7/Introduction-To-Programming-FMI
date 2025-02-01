bb118fe4de4c2e37e6de4aaaa065043002048742
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2025-02-01 17:19:33 +0200

    Create reverse num.cpp

---
+++ Exam prep/reverse num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void ConvertNum(long& num)
+{
+	if (num < 0) return;
+	long newNum = 0;
+
+	while (num != 0)
+	{
+		newNum += num % 10;
+		newNum *= 10;
+		num /= 10;
+	}
+
+	newNum /= 10;
+	num = newNum;
+}
